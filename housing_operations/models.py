0d9d9a3b253a63eb62268dfd5dcf1a101414b41b
Moses <moses.junior@student.moringaschool.com>
2022-03-28 16:47:42 +0300

    adding an app to run housing operations

--- housing_operations/models.py
+++ housing_operations/models.py
@@ -1,3 +1,24 @@
+from django.db.models.deletion import CASCADE, SET_NULL
 from django.db import models
 
-# Create your models here.
+from housing_users.models import Account
+
+class House(models.Model):
+    name = models.CharField(max_length=300,unique=True,null=True),
+    address = models.TextField(null=True),
+    house_category = models.CharField(max_length=200,null=True)
+    number_of_bedrooms = models.IntegerField(null=True)
+    apartments_spaces = models.IntegerField(null=True)
+    apartments_available = models.IntegerField(null=True)
+    house_image = models.ImageField(null=True)
+    livingroom_image = models.ImageField(null=True)
+    kitchen_image = models.ImageField(null=True)
+    sanitaryroom_image = models.ImageField(null=True)
+    bathroom_image = models.ImageField(null=True)
+    bedroom = models.ImageField(null=True)
+    landlord = models.ForeignKey(Account,on_delete=CASCADE,null=True)
+
+class User(models.Model):
+    user = models.OneToOneField(Account,on_delete=CASCADE),
+    house = models.ForeignKey(House,on_delete=SET_NULL),
+    address = models.TextField(null=True)
---
+++ housing_operations/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from housing_operations import views as views
+
+
+
+urlpatterns = [
+    
+]
