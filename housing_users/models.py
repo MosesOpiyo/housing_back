ff8304bb80ec656b09eb46aaec795260e8e9f0da
Moses <moses.junior@student.moringaschool.com>
2022-03-28 16:49:31 +0300

    changing models design to go with housing operations

--- housing_users/models.py
+++ housing_users/models.py
@@ -2,8 +2,10 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager,PermissionsMixin
 from django.conf import settings
 from django.db.models.signals import post_save
+from django.db.models.deletion import CASCADE, SET_NULL
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
+
 
 class MyAccountManager(BaseUserManager):
     """defines the methods to manage the custom user to be created
